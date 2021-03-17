1990f5e1c2185afd489d678979b0646518a16ae6
shalomorlando <orlandostudies@gmail.com>
2021-03-17 17:25:04 +0300

    week one final code

--- app/src/main/java/com/example/weatherman/MainActivity.kt
+++ app/src/main/java/com/example/weatherman/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.weatherman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipCodeEditText: EditText = findViewById(R.id.zipCodeEditText) // we now have a reference to the edit text displayed on the screen. We can now manipulate it as we desire
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            val zipCode: String = zipCodeEditText.text.toString()
+
+            if ( zipCode.length != 5){
+                Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, zipCode, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
